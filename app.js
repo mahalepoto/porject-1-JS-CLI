580a5a96593fa92ad30f387b69ee8a73913f2bc1
Mushtaq A Halepoto <mahalepoto@outlook.com>
2020-07-30 01:57:14 +0100

    CLI Application's all bugs fixed.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const API = require("./lib/API")("./lib/db.json");
 const readlineSync = require("readline-sync");
+const chalk = require("chalk");
 
 // Your code goes here!
 
@@ -41,61 +42,95 @@ const movies = [
     "Pakistan",
   ];
 
-  const topMenu = () => {
-    const categories = ["Books", "Movies", "Next destination"];
-    let categoryIndex = readlineSync.keyInSelect(
-      categories,
-      "Choose your option"
-    );
-    let toMatchIndex = categoryIndex + 1;
-    //console.log("categories index " + toMatchIndex); for testing only
-  
-  };
-  
-  topMenu();
+
+/// main menu function
+const categories = ["Books", "Movies", "Next destination"];
+let userSelectionMatch = 0;
+const topMenu = () => {
+  let categorySelected = readlineSync.keyInSelect(
+    categories,
+    "Choose your option"
+  );
+     userSelectionMatch = categorySelected + 1;
+  console.log("User selected option no: " + userSelectionMatch); //for testing only
+
+};
+
+topMenu();
 
 
-  // displaying books
+// // connecting main menu to books/movies/countries option by invoking their functions 
+// if (userSelectionMatch <= categories.length && userSelectionMatch == 1) {
+//   renderBooksList();
+// } else if (userSelectionMatch <= categories.length && userSelectionMatch == 2) {
+//   renderMoviesList();
+// } else if (userSelectionMatch <= categories.length && userSelectionMatch == 3) {
+//   renderCountriesList();
+// }
+
+
+//   // displaying books
 const renderBooksList = () => {
 	const selectedBookIndex = readlineSync.keyInSelect(
 	  [...books, "Go to main menu"],
 	  "Please select a Book by its number"
-	);
-	console.log('selected book index ' + selectedBookIndex);	
+  );
+  // console.log('selected book index ' + selectedBookIndex);	testing
+  
 	if (selectedBookIndex !== books.length) {
-	  selectedItems.book = books[selectedBookIndex-1];
+    selectedItems.book = books[selectedBookIndex];
+    console.log('Your are going to read ' + chalk.greenBright(selectedItems.book))
 	} else {
 	  topMenu();
 	}
   };
-console.log(renderBooksList());
+  // connecting main menu to books option by invoking their functions 
+  if (userSelectionMatch <= categories.length && userSelectionMatch == 1) {
+    renderBooksList();
+  } 
+
 
 // displaying movies
 const renderMoviesList = () => {
 	const selectedMoviesIndex = readlineSync.keyInSelect(
 	  [...movies, "Go to main menu"],
-	  "Please select a Book by its number"
-	);
+	  "Please select a Movie by its number"
+  );
+  console.log('Movie arrays length ' + movies.length);
 	console.log('selected movies index ' + selectedMoviesIndex);	
-	if (selectedMoviesIndex !== books.length) {
-	  selectedItems.movies = movies[selectedMoviesIndex - 1];
+	if (selectedMoviesIndex !== movies.length) {
+    selectedItems.movie = movies[selectedMoviesIndex];
+    console.log('Your are going to watch movie ' + chalk.red(selectedItems.movie));
 	} else {
 	  topMenu();
 	}
   };
-console.log(renderMoviesList());
+  // connecting main menu to movies option by invoking their functions 
+  if (userSelectionMatch <= categories.length && userSelectionMatch == 2) {
+    renderMoviesList();
+  } 
 
-// displaying countries
+// // displaying countries
 const renderCountriesList = () => {
 	const selectedCountriesIndex = readlineSync.keyInSelect(
 	  [...countries, "Go to main menu"],
-	  "Please select a Book by its number"
+	  "Please select a country by its number"
 	);
 	console.log('selected countries index ' + selectedCountriesIndex);	
 	if (selectedCountriesIndex !== countries.length) {
-	  selectedItems.countries = countries[selectedCountriesIndex - 1];
+    selectedItems.nextTrip = countries[selectedCountriesIndex];
+    console.log("Your traveling to " + chalk.blue(selectedItems.nextTrip));    
 	} else {
 	  topMenu();
 	}
   };
-console.log(renderCountriesList());
+  // connecting main menu to countries option by invoking their functions 
+  if (userSelectionMatch <= categories.length && userSelectionMatch == 3) {
+    renderCountriesList();
+  }
+
+
+
+  
+
+
